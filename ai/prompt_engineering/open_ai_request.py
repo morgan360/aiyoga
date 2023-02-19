5f1c6d29ce0df21bad919667a37b821c3fc15fd5
grainne.mcknight <mcknighg@tcd.ie>
2023-02-19 20:52:42 +0100

    fesemantic searchg

--- ai/prompt_engineering/open_ai_request.py
+++ ai/prompt_engineering/open_ai_request.py
@@ -1,3 +1,6 @@
+import re
+from typing import List
+
 import openai
 from django.conf import settings
 from ai.prompt_engineering import config
@@ -7,6 +10,12 @@ API_KEY = getattr(settings, "OPEN_API_KEY", None)
 
 openai.api_key = API_KEY
 
+
+def pose_name_extraction(answer: str)-> List[str]:
+    """This function extracts the pose names from the answer assuming they begin with a
+     numbered bullet point and end with a colon. """
+    pattern = r'^[0-9*]+\.?\s*(.+?):\s*'
+    return re.findall(pattern, answer, re.MULTILINE)
 
 def prompt_gpt3(skill_level: str, duration: int, question: str):
     response = openai.Completion.create(
@@ -32,5 +41,6 @@ def prompt_gpt3(skill_level: str, duration: int, question: str):
         "duration": duration,
         "prompt": config.PROMPT.format(skill_level, duration, question),
         "answer": answer,
+        "pose_names": pose_name_extraction(answer),
         "image_url": image_url,
     }
